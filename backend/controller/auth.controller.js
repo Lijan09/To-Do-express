4db10c884f2ef5a71f2ae63f47dc2cf279414ee2
Lijan09 <lijanaayasta@gmail.com>
2025-05-23 12:11:00 +0545

    Archi Test

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,4 +1,23 @@
 const generateToken = require("../utils/auth/token");
 const User = require("../models/users.model");
+const catchAsync = require("../utils/catchAsync");
 
-exports.register = 
+exports.register = async (req, res) => {
+  const { name, password, username } = req.body;
+  const user = await User.create({
+    name,
+    password,
+    username,
+  });
+  const token = generateToken(user.username);
+  res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      user: {
+        name: user.name,
+        username: user.username,
+      },
+    },
+  });
+};
